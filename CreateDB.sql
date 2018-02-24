2b803e2268bd0e77d4410fb61279d44bd1161964
xiaoyustudio181 <35361157+xiaoyustudio181@users.noreply.github.com>
2018-02-24 10:53:41 +0800

    20180224.1053

--- CreateDB.sql
+++ CreateDB.sql
@@ -1,5 +1,7 @@
 create database `ProjectDB` default character set utf8 collate utf8_general_ci;
 use `ProjectDB`;
+#mysqldump -uroot -p ProjectDB >D:\backup.sql #备份
+#mysql -u root -p ProjectDB < D:\backup.sql #还原
 #======================================
 create table `table1`
 (
