645bf58f85de0164cefc93aa3e57845370206f45
xiaoyustudio181 <35361157+xiaoyustudio181@users.noreply.github.com>
2018-02-24 15:14:37 +0800

    20180224.1514

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,6 +2,13 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+	public function __construct()
+    {
+        parent::__construct();
+        /*header("Access-Control-Allow-Origin:*");#ajax访问用
+        header("Access-Control-Allow-Methods:GET,POST");#ajax访问用*/
+        $this->table1=M('table1');
+    }
     public function index(){
         $this->display();
     }
