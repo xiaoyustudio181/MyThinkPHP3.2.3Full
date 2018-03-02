77a640b88b76acf2b42f745ba50fc4ae468f9afc
xiaoyustudio181 <35361157+xiaoyustudio181@users.noreply.github.com>
2018-03-02 13:59:26 +0800

    20180302.1358

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -5,4 +5,14 @@ class IndexController extends Controller {
     public function index(){
         $this->display();
     }
+	function successInfo($msg,$data){
+        return $this->buildJson('success',$msg,$data);
+    }
+    function errorInfo($msg){
+        return $this->buildJson('error',$msg,'');
+    }
+    function buildInfo($result,$messsge,$data){
+        $arr=['result'=>$result, 'message'=>$messsge, 'data'=>$data];
+        return json_encode($arr);
+    }
 }
