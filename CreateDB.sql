5399e5f1c28f8a8d1b0e3640805519779fbd4b3a
xiaoyustudio181 <35361157+xiaoyustudio181@users.noreply.github.com>
2018-03-08 14:47:21 +0800

    20180308.1447
    修改sql文件

--- CreateDB.sql
+++ CreateDB.sql
@@ -1,8 +1,8 @@
 create database `ProjectDB` default character set utf8 collate utf8_general_ci;
 #drop database `ProjectDB`;
 use `ProjectDB`;
-#mysqldump -uroot -p ProjectDB >D:\backup.sql #备份
-#mysql -u root -p ProjectDB < D:\backup.sql #还原
+#mysqldump -uroot -p ProjectDB >D:\ProjectDB.bak #备份
+#mysql -u root -p ProjectDB < D:\ProjectDB.bak #还原
 #======================================
 create table `persons`
 (
