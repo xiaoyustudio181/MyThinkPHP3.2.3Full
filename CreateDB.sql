43986a2c45bdaa51dd711d415eb1ce1534686eb0
xiaoyustudio181 <35361157+xiaoyustudio181@users.noreply.github.com>
2018-04-07 16:44:47 +0800

    180407.1644
    修改createDB.sql

--- CreateDB.sql
+++ CreateDB.sql
@@ -1,29 +1,30 @@
-create database `company1` default character set utf8 collate utf8_general_ci;
+#create database `company1` default character set utf8 collate utf8_general_ci;
 #drop database `company1`;
 use `company1`;
 #mysqldump -uroot company1 >D:\company1.bak #备份(-p password)
 #mysql -uroot company1 < D:\company1.bak #还原(-p password)
 #======================================
+drop table if exists `departments`;
 create table `departments`
 (
 	`id` serial primary key,
     `name` nvarchar(10),
     `deleted` boolean default 0
 )engine=innodb;
-insert into `departments`(`name`) values('部门一');
-insert into `departments`(`name`) values('部门二');
+insert into `departments`(`name`) values
+('部门一'),('部门二');
 #======================================
+drop table if exists `employees`;
 create table `employees`
 (
 	`id` serial primary key,
     `departmentid` bigint,
 	`deleted` boolean default 0
 )engine=innodb;
-insert into `employees`(`departmentid`) values(1);
-insert into `employees`(`departmentid`) values(1);
-insert into `employees`(`departmentid`) values(2);
-insert into `employees`(`departmentid`) values(2);
+insert into `employees`(`departmentid`) values
+(1),(1),(2),(2);
 #======================================
+drop table if exists `profiles`;
 create table `profiles`
 (
     `employeeid` bigint unsigned unique,
@@ -31,7 +32,5 @@ create table `profiles`
     `gender` boolean,
     `age` tinyint unsigned
 )engine=innodb;
-insert into `profiles`(`employeeid`,`name`,`gender`,`age`) values(1,'甲',1,29);
-insert into `profiles`(`employeeid`,`name`,`gender`,`age`) values(2,'乙',0,21);
-insert into `profiles`(`employeeid`,`name`,`gender`,`age`) values(3,'丙',1,24);
-insert into `profiles`(`employeeid`,`name`,`gender`,`age`) values(4,'丁',0,32);
+insert into `profiles`(`employeeid`,`name`,`gender`,`age`) values
+(1,'甲',1,29),(2,'乙',0,21),(3,'丙',1,24),(4,'丁',0,32);
